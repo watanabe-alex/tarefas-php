233c882e64b9c8b4a04f77bef8e6d7d95ab8eda7
Alex Watanabe <aowatanabe@gmail.com>
2019-10-18 08:50:52 -0300

    ajustando parte do cookie

--- tarefa9/script/encerrarSessao.php
+++ tarefa9/script/encerrarSessao.php
@@ -2,7 +2,7 @@
     session_start();
     unset($_SESSION);
 
-    //não sei direito pra que isso se durante o exercício não foi setada nenhuma variável em COOKIE
+    //não sei direito pra que isso se durante o exercício não foi setada nenhuma variável em COOKIE pela gente
     unset($_COOKIE["email"]);
 
     header("Location: ../login.php");
